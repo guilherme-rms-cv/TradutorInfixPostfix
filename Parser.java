93163c16bde6514ee519ab8f54745cb321e7f8c1
guilherme-rms-cv <guilherme.rms.cv@gmail.com>
2025-05-19 17:18:50 -0300

    feat: create class Parser with constructor and empty parse method

---
+++ Parser.java
@@ -0,0 +1,12 @@
+public class Parser {
+    private byte[] input;
+    private int current;
+
+    public Parser(byte[] input) {
+        this.input = input;
+    }
+
+    public void parse() {
+        // será implementado depois
+    }
+}
