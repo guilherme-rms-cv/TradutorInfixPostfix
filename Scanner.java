8b46475c1b6c4701a16aab0f025226a7ea24079c
guilherme-rms-cv <guilherme.rms.cv@gmail.com>
2025-05-19 22:05:08 -0300

    feat: update scanner to support tokens of type number with multiple digits

--- Scanner.java
+++ Scanner.java
@@ -20,29 +20,43 @@ public class Scanner {
         }
     }
 
-    public char nextToken() {
-        char ch = peek();
-
-        if (Character.isDigit(ch)) {
+    private Token number() {
+        int start = current;
+        while (Character.isDigit(peek())) {
             advance();
-            return ch;
+        }
+        String n = new String(input, start, current - start);
+        return new Token(TokenType.NUMBER, n);
+    }
+
+    public Token nextToken() {
+        char ch = peek();
+        if (ch == '0') {
+            advance();
+            return new Token(TokenType.NUMBER, Character.toString(ch));
+        } else if (Character.isDigit(ch)) {
+            return number();
         }
 
         switch (ch) {
             case '+':
+                advance();
+                return new Token(TokenType.PLUS, "+");
             case '-':
                 advance();
-                return ch;
+                return new Token(TokenType.MINUS, "-");
+            case '\0':
+                return new Token(TokenType.EOF, "EOF");
             default:
-                break;
+                throw new Error("lexical error at " + ch);
         }
-
-        return '\0';
     }
 
     public static void main(String[] args) {
-        String input = "4-8+6";
+        String input = "289-85+0+69";
         Scanner scan = new Scanner(input.getBytes());
+        System.out.println(scan.nextToken());
+        System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
         System.out.println(scan.nextToken());
